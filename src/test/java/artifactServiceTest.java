158e0a6d499adedbe1ff70029bd3d0e0e42b44a8
Aleksandra <aleksandra.oftin@mail.ru>
2020-02-16 11:42:09 +0300

    Modification

---
+++ src/test/java/artifactServiceTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class artifactServiceTest {
+    @Test
+    void shouldSubmitRequest() {
+
+        open ("http://localhost:9999");
+        SelenideElement form = $("[data-test-id=callback-form]");
+        form.$ ("[data-test-id=name]input").setValue("Василий");
+        form.$ ("[data-test-id=phone]input").setValue("+79270000000");
+        form.$ ("[data-test-id=agreement]").click();
+        form.$ ("[data-test-id=submit]").click();
+        $ (".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+    }
+}
+
