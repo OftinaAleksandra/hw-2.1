9e681e1570af375c5f1fd5b0c2ff2c44458ffd87
Aleksandra <aleksandra.oftin@mail.ru>
2020-02-16 15:23:14 +0300

    Modification method artifactServiceTest

--- src/test/java/artifactServiceTest.java
+++ src/test/java/artifactServiceTest.java
@@ -1,18 +1,22 @@
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import com.codeborne.selenide.Selenide.open;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class artifactServiceTest {
     @Test
     void shouldSubmitRequest() {
 
         open ("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$ ("[data-test-id=name]input").setValue("Василий");
-        form.$ ("[data-test-id=phone]input").setValue("+79270000000");
+
+        SelenideElement form = $("[method=post]");
+        form.$ ("[data-test-id=name] input").setValue("Иванов Иван");
+        form.$ ("[data-test-id=phone] input").setValue("+79270000000");
         form.$ ("[data-test-id=agreement]").click();
-        form.$ ("[data-test-id=submit]").click();
-        $ (".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+        form.$ ("[role=button] ").click();
+        $ ("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
 
